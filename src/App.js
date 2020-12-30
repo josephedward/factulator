9537e5c8c619ff9912ad5af5fb7d7730fb8d22e0
Joe <josephedwardwork@gmail.com>
2020-12-29 21:48:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route,
     // Link
  } from "react-router-dom";
 import Calculator from "./Calculator";
-import Test from "./Test"
+
 
 export default function App(){
 return(
